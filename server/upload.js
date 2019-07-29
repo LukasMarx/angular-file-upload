c29d0b1e2e28b700107029f075f81d1eff7bcbd5
Tosin <kajopelayeola@gmail.com>
2019-07-28 23:00:16 -0400

    Clean up declaration syntax

--- server/upload.js
+++ server/upload.js
@@ -2,14 +2,14 @@ const IncomingForm = require('formidable').IncomingForm;
 const fs = require('fs');
 
 module.exports = function upload(req, res) {
-  var form = new IncomingForm();
-  let readStream;
+  const form = new IncomingForm();
+
   form.on('file', (field, file) => {
     // Do something with the file
     // e.g. save it to the database
     // you can access it using file.path
     console.log('file', file.name);
-    readStream = fs.createReadStream(file.path);
+    const readStream = fs.createReadStream(file.path);
   });
   form.on('end', () => {
     res.json();
